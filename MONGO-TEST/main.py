501e98b6070f17a6115c34d4416b35c9a7dfdec0
Your Name <you@example.com>
2024-05-27 22:07:50 -0300

    configurações

---
+++ MONGO-TEST/main.py
@@ -0,0 +1,24 @@
+client = MongoClient("")
+
+db=client.my_test
+
+tweets_collection = db.tweets
+
+tweets_collection.insert_one({"author": "teste", "text": "texto qualquer"})
+
+tweets = tweets_collection.find({})
+
+print(list(tweets))
+
+BRAZIL_WOE_ID =23424768
+
+auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+
+auth.set_acess_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+trends = api.trends_place(BRAZIL_WOE_ID)
+
+for tweet in trends:
+    print(tweet)
