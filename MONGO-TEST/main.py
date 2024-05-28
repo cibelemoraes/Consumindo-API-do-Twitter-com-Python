7dfce1014029ba2f6a52c396299c86d412192ae0
Your Name <you@example.com>
2024-05-28 09:44:47 -0300

    main

--- MONGO-TEST/main.py
+++ MONGO-TEST/main.py
@@ -13,19 +13,19 @@ import tweepy
 
 #print(list(tweets))
 
-#BRAZIL_WOE_ID =23424768
+BRAZIL_WOE_ID =23424768
 
 auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
 
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-#auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
 
-#auth.set_acess_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+auth.set_acess_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-#api = tweepy.API(auth)
+api = tweepy.API(auth)
 
-#trends = api.trends_place(BRAZIL_WOE_ID)
+trends = api.trends_place(BRAZIL_WOE_ID)
 
-#for tweet in trends:
-#    print(tweet)
+for tweet in trends:
+   print(tweet)
