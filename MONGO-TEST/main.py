29ffeb752305229f53298379a245475e36162408
Your Name <you@example.com>
2024-05-28 09:53:15 -0300

    secrets

--- MONGO-TEST/main.py
+++ MONGO-TEST/main.py
@@ -1,4 +1,4 @@
-from secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+from secrets import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 import tweepy
 
 #client = MongoClient("")
@@ -18,10 +18,6 @@ BRAZIL_WOE_ID =23424768
 auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
 
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
-
-auth.set_acess_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
