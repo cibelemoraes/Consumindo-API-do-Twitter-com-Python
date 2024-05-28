3696db65174dd99ec619aed21613cf9dd0346cdc
Your Name <you@example.com>
2024-05-28 14:20:50 -0300

    main novo

--- MONGO-TEST/main.py
+++ MONGO-TEST/main.py
@@ -1,27 +1,21 @@
-from secrets import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 import tweepy
 
-#client = MongoClient("")
+# Importando as chaves de acesso do arquivo secrets
+from secrets import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
-#db=client.my_test
-
-#tweets_collection = db.tweets
-
-#tweets_collection.insert_one({"author": "teste", "text": "texto qualquer"})
-
-#tweets = tweets_collection.find({})
-
-#print(list(tweets))
-
-BRAZIL_WOE_ID =23424768
-
+# Configurando a autenticação
 auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
-
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
+# Criando uma instância da API
 api = tweepy.API(auth)
+# WOEID do Brasil (esse parâmetro pode não ser necessário na API v2)
+BRAZIL_WOE_ID = 23424768
 
-trends = api.trends_place(BRAZIL_WOE_ID)
+# Obtendo as tendências (verifique o endpoint correto para a API v2)
+try:
+    trends = api.get_place_trends(BRAZIL_WOE_ID)
+    print(trends)
+except tweepy.TweepyException as e:
+    print(f"Error: {e}")
 
-for tweet in trends:
-   print(tweet)
