3ab6a33f3eee46262eed706a8ba781f253e3e0af
Your Name <you@example.com>
2024-05-28 09:14:19 -0300

    readme

--- MONGO-TEST/main.py
+++ MONGO-TEST/main.py
@@ -1,24 +1,31 @@
-client = MongoClient("")
+from secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+import tweepy
 
-db=client.my_test
+#client = MongoClient("")
 
-tweets_collection = db.tweets
+#db=client.my_test
 
-tweets_collection.insert_one({"author": "teste", "text": "texto qualquer"})
+#tweets_collection = db.tweets
 
-tweets = tweets_collection.find({})
+#tweets_collection.insert_one({"author": "teste", "text": "texto qualquer"})
 
-print(list(tweets))
+#tweets = tweets_collection.find({})
 
-BRAZIL_WOE_ID =23424768
+#print(list(tweets))
+
+#BRAZIL_WOE_ID =23424768
 
 auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
 
-auth.set_acess_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-api = tweepy.API(auth)
+#auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
 
-trends = api.trends_place(BRAZIL_WOE_ID)
+#auth.set_acess_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-for tweet in trends:
-    print(tweet)
+#api = tweepy.API(auth)
+
+#trends = api.trends_place(BRAZIL_WOE_ID)
+
+#for tweet in trends:
+#    print(tweet)
--- README.md
+++ README.md
@@ -2,7 +2,9 @@
   <p>DIO: Desafio de Projeto</p>
   <h1>Consumindo a API do Twitter com Python</h1>
 
-  <p>Criar um projeto prático com o objetivo de consumir a API REST do Twitter seguindo uma série de boas práticas e dicas foram apresentadas pelo expert. Entre elas o Tweepy, para reduzir a complexidade no consumo da API do Twitter.</p>
+  <p>Criar um projeto prático com o objetivo de consumir a API REST do Twitter seguindo uma série de boas práticas e dicas foram apresentadas pelo expert. Entre elas o Tweepy, para reduzir a complexidade no consumo da API do Twitter.
+  
+  A API do Twitter permite analisar o desempenho orgânico dos Tweets, permitindo ver uma série de métricas associadas a um determinado Tweet. Isso pode ser útil na tomada de decisões baseadas em dados sobre o desempenho de Tweets específicos. A pesquisa de Tweet é um método GET que retorna informações sobre um Tweet ou grupo de Tweets. As métricas estão disponíveis como parte do endpoint de pesquisa do Tweet.</p>
   <h3>Tecnoligias Utilizadas:</h3>
   <ul>
     <li>Python 3.10</li>
